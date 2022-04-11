66f23e7a621cde666fa95c7dcd96aec03fea2bb7
Saku Kaarakainen <saku.kaarakainen@innofactor.com>
2022-04-11 21:51:03 +0300

    completed entire nft token  contract

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -8,7 +8,17 @@ contract NFT is ERC721URIStorage {
   uint public tokenCount;
 
   // // https://docs.openzeppelin.com/contracts/2.x/api/token/erc721#ERC721
-  constructor() ERC721() {
+  constructor() ERC721('My NFT', 'MYNFT') {
 
+  }
+
+  // memory keyword - with strings you need to specify the memory location of the argument
+  // So in this case it's in memory
+  function mint(string memory _tokenURI) external returns (uint) {
+    tokenCount++;
+
+    _safeMint(msg.sender, tokenCount);
+    _setTokenURI(tokenCount, _tokenURI);
+    return (tokenCount);
   }
 }
